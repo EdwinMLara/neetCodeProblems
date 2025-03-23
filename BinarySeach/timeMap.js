e20b6162ca212fd85fde855bc029cf5591c1cb51
EdwinMLara <emlara35@gmail.com>
2025-03-22 19:50:23 -0600

    explaining the code

--- BinarySeach/timeMap.js
+++ BinarySeach/timeMap.js
@@ -14,17 +14,45 @@
  * Note : For all call to set, the timestamps are in strictly incresing order  
  */
 
+
+/**
+ * There are two main things in this problem to be focused. 
+ * if there is not a key, so  we return an empty string, besides a key can have multiple  
+ * values and these have an timestamp. so when we call the get method if the key exist 
+ * we return the most recent value base on the timetamp. but if the timestamp does not exist 
+ * we return the last create taking account the timestamp value.  
+ */
+
 class TimeMap {
     constructor(){
         this.keyStore = new Map();
+    
+    
     }
 
+    /**
+     * 
+     * @param {string} key 
+     * @param {string} value 
+     * @param {int} timestamp 
+     * 
+     * here we verify if the key doesn't exist in the map at this case we create a new key-value pair with the key and an empty array
+     * once the key is created we added a another array with the value and the timestamp 
+     */
     set(key, value,timestamp){
         if(!this.keyStore.has(key))
             this.keyStore.set(key,[]);
         this.keyStore.get(key).push([value,timestamp]);
     }
 
+    /**
+     * 
+     * @param {string} key 
+     * @param {int} timestamp 
+     * @returns {string}
+     * 
+     * here we're appliying a binary search 
+     */
     get(key, timestamp){
         const values = this.keyStore.get(key) || [];
         console.log(values);
@@ -35,6 +63,12 @@ class TimeMap {
         while(left <= right){
             let mid = Math.floor((left + right)/2);
             console.log(mid);
+            /**
+             * any value that is less than the timestamp is taking as valid result. 
+             * but we need to keep running the loop just in case there is another value 
+             * with a bigger timestamp. this is because we are applying a binary search so when we reduce the search space 
+             * there is a posibility that we can jump a valid value
+             */
             if(values[mid][1] <= timestamp){
                 result = values[mid][0];
                 left = mid + 1;
